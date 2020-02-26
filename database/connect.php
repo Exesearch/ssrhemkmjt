c22b1c8db53699af0f5d6c65811d2984013c6bda
km585 <57662022+km585@users.noreply.github.com>
2020-02-26 11:19:01 +0000

    Update connect.php

--- database/connect.php
+++ database/connect.php
@@ -3,7 +3,7 @@ require_once "./configure.php";
 
 $conn = new mysqli(server, user, pass);
 
-$ss = "CREATE DATABASE IF NOT EXISTS treasure_hunt;";
+$ss = "CREATE DATABASE IF NOT EXISTS exesearch;";
 $conn->query($ss);
 
  ?>
