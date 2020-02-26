ff0f340b76278c40a07d2c75604f89745f4ec9ec
km585 <57662022+km585@users.noreply.github.com>
2020-02-26 12:25:43 +0000

    Update connection.php

--- database/connection.php
+++ database/connection.php
@@ -1,11 +1,18 @@
 <?php
 
+/**
+* This file establish a connection to a specific database in a specific serevr.
+* @author kenta
+*
+*/
+
 require_once "./configure.php";
 require("./connect.php");
 
 $conn = new mysqli(server, user, pass, database);
 
 if ($conn->connect_errno) {
+  //if it failsto connect, error meesage showu up.
   echo "Connection fail...".$conn->connect_error."<br/>";
 }else {
   //echo "Successfully connected! <br/>";
