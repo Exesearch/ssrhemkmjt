ea03244f16bd16e4a436e0b660d3e123f817b35a
nebulous1233 <60700406+nebulous1233@users.noreply.github.com>
2020-03-03 13:17:55 +0000

    Add files via upload

---
+++ database/Registration.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS Registration;
+CREATE DATABASE Registration;
+USE Registration;
+
+  CREATE TABLE IF NOT EXISTS users (
+`ID` INT PRIMARY KEY AUTO_INCREMENT,
+`forename` VARCHAR(45) NOT NULL,
+`surname` VARCHAR(45) NOT NULL,
+`username` VARCHAR(45) NOT NULL,
+`password` VARCHAR(45) NOT NULL,
+`email` VARCHAR(45) NOT NULL,
+`occupation` VARCHAR(45) NOT NULL
+);
+
+
+ 
+
